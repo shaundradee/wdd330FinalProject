d41da4001fe317991cd7b52e33b103307f22b050
SHAUNDRA\shaun <shaundra.d.strong@gmail.com>
2025-12-10 18:33:33 -0500

    One quick change in load order for main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,13 @@
 import { updateFooterYear, setupMenuToggle} from "./utilities.mjs"
 import { initNewsletter, initSpotlight } from "./homepage.mjs";
 
-// Initialize newsletter form functionality
-initNewsletter();
-// Initialize spotlight functionality
-initSpotlight();
+
 // once footer is loaded, update the year
 updateFooterYear();
 // Adding menu toggle functionality
 setupMenuToggle();
+
+// Initialize newsletter form functionality
+initNewsletter();
+// Initialize spotlight functionality
+initSpotlight();
